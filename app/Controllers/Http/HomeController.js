746911cfdea2dead4beb3153e25139bb7d11e79c
Bétourné Alexandre <alexandre.betourne@hotmail.fr>
2019-05-09 14:53:49 +0200

    d

--- app/Controllers/Http/HomeController.js
+++ app/Controllers/Http/HomeController.js
@@ -1,22 +1,54 @@
 'use strict'
 
 const Post = use("App/Models/Post");
+const User = use("App/Models/User");
 const axios = require('axios');
 
 
 class HomeController {
 	async index({ view }) {
-		const posts = []
+		var postsPromise = await Post.query().fetch()
+		var usersPromise = await User.query().fetch()
+		var posts = []
+		var users = []
 
-		await Post.all().then(p => {
-			p.rows.forEach(row => {
-				posts.push({
-					title: row.$attributes.title,
-					body: row.$attributes.body,
-					href: row.$attributes.slug
-				})
-			})
+		usersPromise.rows.forEach(u => {
+			users.push(u.$attributes)
 		})
+
+		postsPromise.rows.forEach(p => {
+			users.forEach(u => {
+				if (u.id === p.$attributes.user_id) {
+					posts.push({
+						title: p.$attributes.title,
+						body: p.$attributes.body,
+						user: u.username,
+						href: p.$attributes.slug,
+						update: p.$attributes.updated_at
+					})
+				}
+			})
+
+		})
+
+
+		// await Post.all().then(p => {
+		// 	p.rows.forEach(postRow => {
+		// 		User.all().then(u => {
+		// 			u.rows.forEach(userRow => {
+		//
+		// 				if (postRow.$attributes.user_id == userRow.$attributes.id) {
+		// 					posts.push({
+		// 						title: postRow.$attributes.title,
+		// 						body: postRow.$attributes.body,
+		// 						user: userRow.$attributes.username,
+		// 						href: postRow.$attributes.slug
+		// 					})
+		// 				}
+		// 			})
+		// 		})
+		// 	})
+		// })
 
 		return view.render("home", { posts: posts });
 	}
